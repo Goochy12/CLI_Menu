cdb2e4032c1ddd4e0c273d255a68d2472e6a7713
Liam-Desktop <liamgooch@live.com.au>
2020-05-01 17:46:25 +1000

    add test variables

--- Menu/main.cpp
+++ Menu/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include "Menu/menu.h"
+#include "MenuItem/menuItem.h"
 
 int main() {
 
-	std::vector<int> menuItems;
-	Menu menu(menuItems);
+	Menu menu("Example Title", "Welcome to my Menu!", "Goodbye!");
+
+	menu.addMenuItem("Menu Item 1", "Numpad 1", false, "OFF", 1);
+
+
+	//menu.display();
 
 	return 0;
 }
